dacd52a91d9d7a67964b9ecda3962913f646090a
chinteck847 <173798885+chinteck847@users.noreply.github.com>
2024-08-18 19:47:06 +0800

    CTLMS96-SeleniumTesting

--- src/test/java/com/ctlms/LoginPageTest.java
+++ src/test/java/com/ctlms/LoginPageTest.java
@@ -29,7 +29,7 @@ class LoginPageTest {
     @BeforeEach
     void setup() {        
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
 
